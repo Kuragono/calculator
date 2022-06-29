cf131873ebdf48ad48c331773f9c9cb5a7c09033
Kuragono <tiago.carreira@outlook.com>
2022-06-30 01:47:16 +0200

    Add large number functionality

--- main.js
+++ main.js
@@ -9,26 +9,36 @@ let stored_operator;
 
 function add(num) {
     display_value += num;
+    DISPLAY.textContent = display_value;
+    stored_number_value = 0;
 }
 
 function subtract(num) {
     display_value -= num;
+    DISPLAY.textContent = display_value;
+    stored_number_value = 0;
 }
 
 function multiply(num) {
     display_value *= num;
+    DISPLAY.textContent = display_value;
+    stored_number_value = 0;
 }
 
 function divide(num) {
     display_value /= num;
+    DISPLAY.textContent = display_value;
+    stored_number_value = 0;
 }
 
 function clear() {
     display_value = 0;
+    stored_number_value = 0;
     DISPLAY.textContent = `${display_value}`;
 }
 
 function operate(operator, num) {
+    num = parseInt(num);
     if (operator === 'ADD') {
         add(num);
     } else if (operator === 'SUBTRACT') {
@@ -39,6 +49,7 @@ function operate(operator, num) {
         divide(num);
     }
     DISPLAY.textContent = `${display_value}`;
+    stored_number_value = 0;
 }
 
 /**** Buttons ****/
@@ -46,7 +57,8 @@ function operate(operator, num) {
 const numbers = document.querySelectorAll('.number');
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].onclick = (a) => {
-        stored_number_value = a.currentTarget.innerText;
+        stored_number_value += a.currentTarget.innerText.toString();
+        DISPLAY.textContent = `${parseInt(stored_number_value)}`;
     }
 }
 
@@ -54,17 +66,21 @@ const operators = document.querySelectorAll('.operator');
 for (let i = 0; i < operators.length; i++) {
     operators[i].onclick = (a) => {
         stored_operator = a.currentTarget.innerText;
+        stored_number_value = parseInt(stored_number_value);
         if (stored_operator == '+') {
             stored_operator = 'ADD';
+            add(stored_number_value);
         } else if (stored_operator == '-') {
             stored_operator = 'SUBTRACT';
+            subtract(stored_number_value);
         } else if (stored_operator == '*') {
             stored_operator = 'MULTIPLY';
+            multiply(stored_number_value);
         } else if (stored_operator == '/') {
-            stored_operator = 'DIVIDE';
+            divide(stored_number_value);
         }
     }  
 }
 
-document.getElementById('btn=').onclick = () => {operate(stored_operator,stored_number_value)};
+document.getElementById('btn=').onclick = () => {operate(stored_operator,parseInt(stored_number_value))};
 document.getElementById('btnc').onclick = () => {clear()};
